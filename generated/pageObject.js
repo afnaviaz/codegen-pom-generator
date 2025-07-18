afadb591ad5f4ed47b245dec54a8b4a4626bc2a4
Andres Navia <andres.navia@finkargo.com>
2025-07-18 13:22:57 -0500

    feat: POM generado automáticamente

--- generated/pageObject.js
+++ generated/pageObject.js
@@ -1,12 +1,7 @@
 class GeneratedPage {
   constructor(page) { this.page = page; }
 
-  async goto_httpsapptesting() { await page.goto('https://app-testing.finkargo.com.mx/auth/login'); }
-  async locator_labelfilterhasT() { await page.locator('label').filter({ hasText: 'Usuario *' }).dblclick(); }
-  async getByRole_textbox() { await page.getByRole('textbox', { name: 'Usuario *' }).click(); }
-  async getByRole_textbox() { await page.getByRole('textbox', { name: 'Usuario *' }).fill('fsdaf'); }
-  async locator_labelfilterhasT() { await page.locator('label').filter({ hasText: 'Contraseña *' }).click(); }
-  async getByRole_textbox() { await page.getByRole('textbox', { name: 'Contraseña *' }).fill('sadfadsf'); }
   async getByRole_button() { await page.getByRole('button', { name: 'Ingresar' }).click(); }
+  async getByRole_button() { await page.getByRole('button', { name: 'Descartar' }).click(); }
 }
 module.exports = { GeneratedPage };
--- generated/test.spec.js
+++ generated/test.spec.js
@@ -3,11 +3,6 @@ const { GeneratedPage } = require('./pageObject');
 
 test('basic flow', async ({ page }) => {
   const generatedPage = new GeneratedPage(page);
-  await generatedPage.goto_httpsapptesting();
-  await generatedPage.locator_labelfilterhasT();
-  await generatedPage.getByRole_textbox();
-  await generatedPage.getByRole_textbox();
-  await generatedPage.locator_labelfilterhasT();
-  await generatedPage.getByRole_textbox();
+  await generatedPage.getByRole_button();
   await generatedPage.getByRole_button();
 });
