e1a4333e323baeade693059d4fd3718325a383ec
Andres Navia <andres.navia@finkargo.com>
2025-07-18 13:22:42 -0500

    feat: POM generado automáticamente

--- generated/test.spec.js
+++ generated/test.spec.js
@@ -3,6 +3,11 @@ const { GeneratedPage } = require('./pageObject');
 
 test('basic flow', async ({ page }) => {
   const generatedPage = new GeneratedPage(page);
-  await generatedPage.getByRole_button();
+  await generatedPage.goto_httpsapptesting();
+  await generatedPage.locator_labelfilterhasT();
+  await generatedPage.getByRole_textbox();
+  await generatedPage.getByRole_textbox();
+  await generatedPage.locator_labelfilterhasT();
+  await generatedPage.getByRole_textbox();
   await generatedPage.getByRole_button();
 });
